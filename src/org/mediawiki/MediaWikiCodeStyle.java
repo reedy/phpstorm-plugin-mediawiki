48339f4d41badc910302d9b20cfbb77a89b0ea66
Sam Reed <reedy@wikimedia.org>
2014-03-26 04:10:22 +0000

    Initial commit
    Currently unworking due to:
    
    Throwable: Assertion failed: Duplicate bundled template PHP Getter Method.php
    Throwable: Language with ID 'PHP' is already registered: class com.jetbrains.php.lang.PhpLanguage

---
+++ src/org/mediawiki/MediaWikiCodeStyle.java
@@ -0,0 +1,22 @@
+package org.mediawiki;
+
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.codeStyle.PredefinedCodeStyle;
+import com.jetbrains.php.lang.PhpFileType;
+import com.jetbrains.php.lang.PhpLanguage;
+
+/**
+ * @author Reedy
+ */
+public class MediaWikiCodeStyle  extends PredefinedCodeStyle {
+    public MediaWikiCodeStyle() {
+        super("MediaWiki", PhpLanguage.INSTANCE);
+    }
+
+    @Override
+    public void apply(CodeStyleSettings settings) {
+        CodeStyleSettings.IndentOptions indentOptions = settings.getIndentOptions(PhpFileType.INSTANCE);
+        indentOptions.USE_TAB_CHARACTER = true;
+        indentOptions.SMART_TABS = true;
+    }
+}
