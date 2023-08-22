d0b41cc8f6ef0a42f857ae9b7edb316188bc99a0
Reedy <reedy@wikimedia.org>
2023-08-22 15:46:40 +0100

    Allow MediaWikiSearchDocsAction to work while indexing etc

--- src/main/java/org/mediawiki/MediaWikiSearchDocsAction.java
+++ src/main/java/org/mediawiki/MediaWikiSearchDocsAction.java
@@ -64,6 +64,6 @@ public class MediaWikiSearchDocsAction extends AnAction {
     }
 
     public @NotNull ActionUpdateThread getActionUpdateThread() {
-        return ActionUpdateThread.BGT;
+        return ActionUpdateThread.EDT;
     }
 }
